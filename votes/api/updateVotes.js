b9fd916d7a9ac7fdc8993fc5abdca2993d9961dc
Yurii <46454397+yur4ik1@users.noreply.github.com>
2024-09-20 18:58:10 +0300

    Update updateVotes.js

--- votes/api/updateVotes.js
+++ votes/api/updateVotes.js
@@ -11,10 +11,10 @@ export default async function handler(req, res) {
         const { id, votes } = req.body;
 
         try {
-            const response = await fetch(`https://mockapi.io/endpoint/features/${id}`, {
+            const response = await fetch(`https://66ed47bb380821644cdc47f2.mockapi.io/features/${id}`, {
                 method: 'PUT',
                 headers: {
-                    'Content-Type': 'application/json',
+                    'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({ votes })
             });
@@ -26,11 +26,11 @@ export default async function handler(req, res) {
             const updatedItem = await response.json();
             res.status(200).json({ message: 'Votes updated successfully', updatedItem });
         } catch (error) {
+            console.error(error);
             res.status(500).json({ error: 'Failed to update votes' });
         }
-    } else if (req.method === 'GET') {
-        res.status(405).json({ message: 'GET requests are not allowed. Use POST.' });
     } else {
+        res.setHeader('Allow', ['POST', 'OPTIONS']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }
 }
